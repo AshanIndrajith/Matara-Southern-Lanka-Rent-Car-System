f319a730fd3b58ff822a9086f2d873443986467a
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-05-14 15:54:27 +0530

    update

---
+++ rent car system/BackEnd/CarRentSystem/src/test/java/com/example/carrentsystem/CarRentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
