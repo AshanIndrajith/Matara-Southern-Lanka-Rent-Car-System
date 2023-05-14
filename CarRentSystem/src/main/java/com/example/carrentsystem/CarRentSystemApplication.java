cc6c92151347cb42c7ecc4e839c9de688679531f
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2023-05-14 15:57:55 +0530

    up

---
+++ CarRentSystem/src/main/java/com/example/carrentsystem/CarRentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentSystemApplication.class, args);
+    }
+
+}
