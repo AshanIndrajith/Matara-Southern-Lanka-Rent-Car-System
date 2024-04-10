7ac96c7867c152972f4dc48d6f8a79b3006ca14a
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2024-04-10 12:35:38 +0530

    update

--- rent car system/system/JwtApplication/src/main/java/com/example/jwtapplication/Controller/AuthController.java
+++ rent car system/system/JwtApplication/src/main/java/com/example/jwtapplication/Controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
